bdc645ccbcd325e03277a5fc35734a46797abd39
Urvish20 <urvishpatel1520@gmail.com>
2024-02-16 17:38:20 +0530

    complete signin page functionality

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,11 +1,79 @@
-import React from 'react'
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
-const SignIn = () => {
+export default function SignIn() {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError(null); // Reset error state on new submission
+    setLoading(true);
+    try {
+      const res = await fetch('/api/auth/signin', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (!res.ok) { // Check if the response status is not OK (e.g., 400 or 500 errors)
+        throw new Error(data.message || 'An error occurred. Please try again.'); // Use the backend's error message or a default one
+      }
+      if (data.success === false) {
+        throw new Error(data.message || 'An error occurred. Please try again.');
+      }
+      // If everything went fine, navigate to Sign-In page
+      navigate('/');
+    } catch (error) {
+      setError(error.message); // Set the error message to display to the user
+    } finally {
+      setLoading(false); // Ensure loading is set to false at the end of the operation
+    }
+  };
   return (
-    <div>
-      <h1>SignIn page</h1>
-    </div>
-  )
-}
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+        <input
+          type='email'
+          placeholder='email'
+          className='border p-3 rounded-lg'
+          id='email'
+          onChange={handleChange}
+        />
+        <input
+          type='password'
+          placeholder='password'
+          className='border p-3 rounded-lg'
+          id='password'
+          onChange={handleChange}
+        />
 
-export default SignIn
+        <button
+          disabled={loading}
+          className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
+        >
+          {loading ? 'Loading...' : 'Sign In'}
+        </button>
+       
+      </form>
+      <div className='flex gap-2 mt-5'>
+        <p>dont have an account?</p>
+        <Link to={'/Sign-Up'}>
+          <span className='text-blue-700'>Sign up</span>
+        </Link>
+      </div>
+      {error && <p className='text-red-500 mt-5'>{error}</p>}
+    </div>
+  );
+}
